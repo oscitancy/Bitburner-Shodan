b28eb614e45ce4cd05da12017c1a1005d2677182
Emily <oscitancy@users.noreply.github.com>
2022-03-26 03:53:09 +0000

    ~ Refactored map.js

--- tools/map.js
+++ tools/map.js
@@ -1,40 +1,27 @@
-var MAX_DEPTH = 20;
-var knownHosts = null;
-
 /** @param {NS} ns **/
 export async function main(ns)
 {
 	ns.disableLog('disableLog');
 	ns.disableLog("scan");
 
-	var count = 0;
 	var depth = 0;
-	var indent = "";
-	var host = ns.getServer();
-	knownHosts = new Set();
-	knownHosts.add(host['hostname']);
-	count = count + mapHost(ns, depth, indent, knownHosts, host);
-	ns.tprint("Total found hosts: " + count);
+	var hostname = 'home';
+	var knownHostnames = new Set();
+	knownHostnames.add(hostname);
+	mapHost(ns, hostname, knownHostnames, depth);
+	ns.tprint("Total found hosts: " + [...knownHostnames].length);
 }
 
-function mapHost(ns, depth, indent, knownHosts, host)
+function mapHost(ns, hostname, knownHostnames, depth)
 {
-	knownHosts.add(host['hostname']);
-	var count = 0;
-	depth += 1;
-	ns.tprintf("%1$s\t%2$s%3$s (%4$s)",
-		depth-1,
-		indent,
-		host['hostname'],
-		host['ip']
-	);
-	var links = ns.scan(host['hostname']);
+	knownHostnames.add(hostname);
+	var ip = ns.getServer(hostname)['ip'];
+	var indent = '|   '.repeat(depth);
+	ns.tprintf("%s\t%s%s (%s)", depth, indent, hostname, ip);
+	var links = ns.scan(hostname);
+	links.shift(); // Remove parent link
 	links.forEach((link) =>
 	{
-		if (!knownHosts.has(link) && depth <= MAX_DEPTH-1)
-		{
-			count = count + 1 + mapHost(ns, depth, indent + "|   ", knownHosts, ns.getServer(link));
-		}
+		mapHost(ns, link, knownHostnames, depth+1);
 	});
-	return count;
 }
