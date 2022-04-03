5f39ba219fcc47100d756fa9138b266685bd8d31
Emily <oscitancy@users.noreply.github.com>
2022-04-03 22:37:25 +0100

    ~ Change map.js to allow silent mapping + Add export mapped hostnames to data store in map.js

--- tools/map.js
+++ tools/map.js
@@ -1,15 +1,23 @@
+const HOSTNAMES_DATA_PATH = '/data/hostnames.dat';
+
+var silent = false;
+
 /** @param {NS} ns **/
 export async function main(ns)
 {
 	ns.disableLog('disableLog');
 	ns.disableLog("scan");
 
+	if (ns.args[0] == "silent") {silent = true;}
+
 	var depth = 0;
 	var hostname = 'home';
 	var knownHostnames = new Set();
 	knownHostnames.add(hostname);
 	mapHost(ns, hostname, knownHostnames, depth);
-	ns.tprint("Total found hosts: " + [...knownHostnames].length);
+	var json = JSON.stringify([...knownHostnames]);
+	await ns.write(HOSTNAMES_DATA_PATH, json, 'w');
+	if (!silent) ns.tprint("Total found hosts: " + [...knownHostnames].length);
 }
 
 function mapHost(ns, hostname, knownHostnames, depth)
@@ -17,7 +25,7 @@ function mapHost(ns, hostname, knownHostnames, depth)
 	knownHostnames.add(hostname);
 	var ip = ns.getServer(hostname)['ip'];
 	var indent = '|   '.repeat(depth);
-	ns.tprintf("%s\t%s%s (%s)", depth, indent, hostname, ip);
+	if (!silent) ns.tprintf("%s\t%s%s (%s)", depth, indent, hostname, ip);
 	var links = ns.scan(hostname);
 	links.forEach((link) =>
 	{
