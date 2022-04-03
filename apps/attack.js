adae2c244ad7f65b17f3df236d6e47015abc5097
Emily <oscitancy@users.noreply.github.com>
2022-04-03 22:38:18 +0100

    ~ Switch around arguments in attack.js

--- apps/attack.js
+++ apps/attack.js
@@ -1,21 +1,22 @@
 /** @param {NS} ns **/
 export async function main(ns)
 {
-	var type = ns.args[0];
-	var host = ns.args[1];
+	var target = ns.args[0];
+	var type = ns.args[1];
 	switch (type)
 	{
 		case 'weaken':
-			while(true){await ns.weaken(host);}
+			while(true){await ns.weaken(target);}
 			break;
 		case 'grow':
-			while(true){await ns.grow(host);}
+			while(true){await ns.grow(target);}
 			break;
 		case 'hack':
-			while(true){await ns.hack(host);}
+			while(true){await ns.hack(target);}
 			break;
 		default:
-			ns.print("Unknown attack type!");
+			ns.tail();
+			ns.print("Unknown attack type! " + type);
 			break;
 	}
 }
