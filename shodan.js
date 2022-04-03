ffb51fbcba935a462dacf338a3664c0b17ae87fc
Emily <oscitancy@users.noreply.github.com>
2022-04-03 22:48:14 +0100

    ~ Changed shodan.js to use premapped hostnames instead of remapping with copied code. ~ Changed copied string literals to a const to reduce head scratching and confusion. ~ Refactored several variables. ~ Reduced output of microstatus function as most of it is on servermon. + Added an autophreak function to automagically call a seperate background script for external server hacking

--- shodan.js
+++ shodan.js
@@ -1,154 +1,182 @@
-import * as Status from "status.js";
-import * as Phreak from "phreak.js";
+const APP_ATTACK_PATH = '/apps/attack.js';
 
-var HIGH_DIFFICULTY_MULTIPLIER = 1.2;
-var LOW_MONEY_MULTIPLIER = 0.95;
+const HOSTNAMES_DATA_PATH = '/data/hostnames.dat';
+const HIGH_DIFFICULTY_MULTIPLIER = 1.2;
+const LOW_MONEY_MULTIPLIER = 0.95;
 
-var hostsmeta = new Set();
+var hostnames = null;
+var servers = null;
+var player = null;
 
-/** @param {NS} ns **/
+///** @param {NS} ns **/
+/** @param {import(".").NS } ns */
 export async function main(ns)
 {
 	ns.disableLog('disableLog');
+	ns.disableLog("scan");
 	ns.disableLog('scp');
 	ns.disableLog('killall');
 	ns.disableLog('sleep');
 	ns.disableLog('exec');
 	ns.disableLog('getHackingLevel');
 
+	ns.clearLog();
 	ns.tail();
 
-	getAllHostnames(ns);
+	servers = new Set();
+	await ns.exec('/tools/map.js', 'home', 1, 'silent');
+	fetchHostnames(ns);
 	rebuildMeta(ns);
+
 	while(true)
 	{
 		ns.clearLog();
-		for (const hostname of new Array(...hostsmeta))
+		fetchHostnames(ns);
+		player = ns.getPlayer();
+
+		for (const server of new Array(...servers))
 		{
-			var host = ns.getServer(hostname);
-			Phreak.phreak(ns, host);
-			//if (ns.getHackingLevel() >= (host['requiredHackingSkill'] / 3))
+			servers[server]['state'] = ns.getServer(server);
+			var state = servers[server]['state'];
+			var hackable = ns.getHackingLevel() >= state['requiredHackingSkill'];
+			var hasRam = state['maxRam'] > 0;
+
+			await autophreak(ns, server);
+			if (hasRam) microStatus(ns, server);
+			if (hackable && hasRam)
 			{
-				microStatus(ns, host, hostsmeta[hostname]);
-				var hasRam = host['maxRam'] > 0;
-				//var hackable = ns.getHackingLevel() >= host['requiredHackingSkill'] / 2;
-				if (hasRam)// && hackable)
-				{
-					await autoAttack(ns, host);
-				}
+				await autoAttack(ns, server);
 			}
 		}
-		await ns.sleep(1000);
+		await ns.sleep(2000);
 	}
 }
 
-function getAllHostnames(ns)
+function fetchHostnames(ns)
 {
-	ns.disableLog("scan");
-	mapHost(ns, 'home');
-	hostsmeta.delete('home');
-	hostsmeta.delete('darkweb');
-	for (var i = 1; i <= 25; i++)
-	{
-		hostsmeta.delete('sv-' + i.toString().padStart(2, '0'));
-	}
-	ns.print("Total found hosts: " + new Array(...hostsmeta).length);
+	var data = ns.read(HOSTNAMES_DATA_PATH);
+    hostnames = JSON.parse(data);
 }
 
-function mapHost(ns, hostname)
+function removeIntra(ns)
 {
-	hostsmeta.add(hostname);
-	hostsmeta[hostname] = new Set();
-	hostsmeta[hostname]['pid'] = 0;
-	hostsmeta[hostname]['attackType'] = '    ';
-	var links = ns.scan(hostname);
-	links.forEach((link) =>
-	{
-		if (!hostsmeta.has(link))
-		{
-			mapHost(ns, link);
-		}
-	});
+	// hostsmeta.delete('home');
+	// hostsmeta.delete('darkweb');
+	// for (var i = 1; i <= 25; i++)
+	// {
+	// 	hostsmeta.delete('sv-' + i.toString().padStart(2, '0'));
+	// }
 }
 
 function rebuildMeta(ns)
 {
-	for (const hostname of new Array(...hostsmeta))
+	hostnames.forEach((hostname) =>
 	{
-		var host = ns.getServer(hostname);
-		var isHack   = ns.print(ns.getRunningScript('attack.js', hostname, 'hack',   hostname));
-		var isGrow   = ns.print(ns.getRunningScript('attack.js', hostname, 'grow',   hostname));
-		var isWeaken = ns.print(ns.getRunningScript('attack.js', hostname, 'weaken', hostname));
+		servers.add(hostname);
+		servers[hostname] = new Set();
 
-		if (isHack != null)
+		var serverState = ns.getServer(hostname);
+		servers[hostname]['state'] = serverState;
+
+		var isHack   = ns.getRunningScript(APP_ATTACK_PATH, hostname, hostname, 'hack');
+		var isGrow   = ns.getRunningScript(APP_ATTACK_PATH, hostname, hostname, 'grow');
+		var isWeaken = ns.getRunningScript(APP_ATTACK_PATH, hostname, hostname, 'weaken');
+
+		if (isHack)
 		{
-			hostsmeta[hostname]['pid'] = isHack['pid'];
-			hostsmeta[hostname]['attackType'] = 'hack';
+			servers[hostname]['pid'] = isHack['pid'];
+			servers[hostname]['attackType'] = 'hack';
 		}
-		else if (isGrow != null)
+		else if (isGrow)
 		{
-			hostsmeta[hostname]['pid'] = isGrow['pid'];
-			hostsmeta[hostname]['attackType'] = 'grow';
+			servers[hostname]['pid'] = isGrow['pid'];
+			servers[hostname]['attackType'] = 'grow';
 		}
-		else if (isWeaken != null)
+		else if (isWeaken)
 		{
-			hostsmeta[hostname]['pid'] = isWeaken['pid'];
-			hostsmeta[hostname]['attackType'] = 'weaken';
+			servers[hostname]['pid'] = isWeaken['pid'];
+			servers[hostname]['attackType'] = 'weaken';
 		}
-		else if (host['maxRam'] === 0)
+		else if (serverState['maxRam'] <= 0)
 		{
-			hostsmeta[hostname]['pid'] = '-';
-			hostsmeta[hostname]['attackType'] = '----';
+			servers[hostname]['pid'] = '-';
+			servers[hostname]['attackType'] = '-';
 		}
-	}
+		else
+		{
+			servers[hostname]['pid'] = '-';
+			servers[hostname]['attackType'] = 'idle';
+		}
+	});
+
 }
 
-function microStatus(ns, host, metadata)
+function microStatus(ns, hostname)
 {
-	var portFlags = "";
-	portFlags += (host['hasAdminRights']) ? 'AD ' : '   ';
-	portFlags += (host['backdoorInstalled']) ? 'BD ' : '   ';
-	portFlags += (host['sshPortOpen']) ? 'SH ' : '   ';
-	portFlags += (host['ftpPortOpen']) ? 'FT ' : '   ';
-	portFlags += (host['httpPortOpen']) ? 'HT ' : '   ';
-	portFlags += (host['smtpPortOpen']) ? 'SM ' : '   ';
-	portFlags += (host['sqlPortOpen']) ? 'SQ' : '  ';
+	var state = servers[hostname]['state'];
 
-	var difficulty = (host['hackDifficulty'] - host['minDifficulty']).toFixed(2).padStart(5, '0');
-	if (host['baseDifficulty'] === 0) { difficulty = '--.--'; }
-	var moneyPercentage = ((100 / host['moneyMax']) * host['moneyAvailable']).toFixed(2).padStart(5, '0');
-	if (host['moneyMax'] === 0) { moneyPercentage = '--.--'; }
-	ns.printf("%s %s [%s] %s %s [%s]",
-		host['hostname'].padEnd(18),
-		("(" + host['ip'] + ")").padEnd(10),
-		portFlags,
+	var difficulty = (state['hackDifficulty'] - state['minDifficulty']).toFixed(2).padStart(5, '0');
+	if (state['baseDifficulty'] === 0) { difficulty = '--.--'; }
+
+	var moneyPercentage = ((100 / state['moneyMax']) * state['moneyAvailable']).toFixed(2).padStart(5, '0');
+	if (state['moneyMax'] === 0) { moneyPercentage = '--.--'; }
+
+	ns.printf("%s %s %s %s [%s]",
+	state['hostname'].padEnd(18),
+		("(" + state['ip'] + ")").padEnd(10),
 		difficulty.padEnd(6),
 		(moneyPercentage + "%").padEnd(7),
-		metadata['attackType']
+		servers[hostname]['attackType']
 	);
 }
 
-async function autoAttack(ns, host)
+async function autophreak(ns, hostname)
 {
-	if (host['maxRam'] === 0) return;
+	var state = servers[hostname]['state'];
+	var args = new Array();
+	args.push(hostname);
+	if (!state['sshPortOpen']) args.push('ssh');
+	if (!state['ftpPortOpen']) args.push('ftp');
+	if (!state['httpPortOpen']) args.push('http');
+	if (!state['smtpPortOpen']) args.push('smtp');
+	if (!state['sqlPortOpen']) args.push('sql');
+	var nukeAvailable = state['openPortCount'] > state['numOpenPortsRequired'];
+	if (!state['hasAdminRights'] && nukeAvailable) args.push('nuke');
+	//if (!state['backdoorInstalled']) args.push('backdoor');
+	if (args.length > 1)
+	{
+		await ns.exec("/apps/phreak.js", 'home', 1, ...args);
+	}
+}
 
-	var maxThreads = host['maxRam'] / 2;
-	var highDifficulty = host['hackDifficulty'] >= (host['minDifficulty'] * HIGH_DIFFICULTY_MULTIPLIER);
-	var lowMoney = host['moneyAvailable'] <= (host['moneyMax'] * LOW_MONEY_MULTIPLIER);
+async function autoAttack(ns, hostname)
+{
+	var state = servers[hostname]['state'];
+	if (state['maxRam'] === 0) return;
+
+	var maxThreads = state['maxRam'] / 2;
+	var lowSkill = state['requiredHackingSkill'] > player.hacking;
+	var lowMoney = state['moneyAvailable'] <= (state['moneyMax'] * LOW_MONEY_MULTIPLIER);
+	var highDifficulty = state['hackDifficulty'] >= (state['minDifficulty'] * HIGH_DIFFICULTY_MULTIPLIER);
 	var attackType = 'hack';
+	if (lowSkill) { attackType = 'lowskill'}
 	if (lowMoney) { attackType = 'grow'; }
 	if (highDifficulty) { attackType = 'weaken'; }
-	if (!ns.fileExists('attack.js', host['hostname']))
+	//if (!ns.fileExists(APP_ATTACK_PATH, hostname))
 	{
-		ns.print("    Pushing attack script...");
-		await ns.scp("attack.js", host['hostname']);
+		if (!ns.fileExists(APP_ATTACK_PATH, 'home'))
+		{
+			ns.print("Attack app is missing!");
+			ns.exit();
+		}
+		await ns.scp(APP_ATTACK_PATH, hostname);
 	}
-	if (attackType != hostsmeta[host['hostname']]['attackType'])
+	if (attackType != servers[hostname]['attackType'])
 	{
-		//ns.print("    Changing attack type to: " + attackType);
-		ns.scriptKill('attack.js', host['hostname']);
-		var pid = ns.exec("attack.js", host['hostname'], maxThreads, attackType, host['hostname']);
-		hostsmeta[host['hostname']]['pid'] = pid;
-		hostsmeta[host['hostname']]['attackType'] = attackType;
+		ns.print("    Changing attack type to: " + attackType);
+		ns.scriptKill(APP_ATTACK_PATH, hostname);
+		var pid = ns.exec(APP_ATTACK_PATH, hostname, maxThreads, hostname, attackType);
+		servers[hostname]['pid'] = pid;
+		servers[hostname]['attackType'] = attackType;
 	}
 }
